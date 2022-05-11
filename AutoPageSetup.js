6056dd76484680e206b59c63c2e282919c5613c3
Stiathirs the Protogen <92652408+StiathirsTheProtogen@users.noreply.github.com>
2022-05-11 12:37:16 -0500

    a

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -1,5 +1,2 @@
 document.getElementById("title").innerHTML = document.title.replace(" - Basic RPG Official Wiki","");
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a href="$1">$1</a>');
-document.body.innerHTML = document.body.innerHTML.replace(/_(.*?)_/g,'<i>$1</i>');
-document.body.innerHTML = document.body.innerHTML.replace(/__(.*?)__/g,'<b>$1</b>');
-document.body.innerHTML = document.body.innerHTML.replace(/___(.*?)___/g,'<b><i>$1</i></b>');
