81e60c5b71ba942db007804ed767a22e31fb1a0a
Stiathirs the Protogen <92652408+StiathirsTheProtogen@users.noreply.github.com>
2022-05-11 12:35:10 -0500

    make pages work by removing blockquote syntax

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -3,4 +3,3 @@ document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a h
 document.body.innerHTML = document.body.innerHTML.replace(/_(.*?)_/g,'<i>$1</i>');
 document.body.innerHTML = document.body.innerHTML.replace(/__(.*?)__/g,'<b>$1</b>');
 document.body.innerHTML = document.body.innerHTML.replace(/___(.*?)___/g,'<b><i>$1</i></b>');
-document.body.innerHTML = document.body.innerHTML.replace(/>(.*?)/g,'<blockquote>$1</blockquote>');
