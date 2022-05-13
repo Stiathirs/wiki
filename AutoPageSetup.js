aa9a7e599c4fdf0711cfcfdbd0cc581bc588183f
Stiathirs the Protogen <92652408+StiathirsTheProtogen@users.noreply.github.com>
2022-05-13 12:03:40 -0500

    Update AutoPageSetup.js

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -1,4 +1,12 @@
+// Ignore errors, this is for the lack of an element with id "title"
+window.onerror = function(){
+  return true;
+}
+
+// Set up page title using title element
 document.getElementById("title").innerHTML = document.title.replace(" - Basic RPG Official Wiki","");
+
+// Basic MD Rules and using a little MediaWiki syntax
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\|(.*?)\]\]/g,'<a href="$2">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a href="$1">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/---/g,'<hr>');
