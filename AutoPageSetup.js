adb6ad0dbcd54241be98bfed7db39f3c1a5ff70d
Stiathirs the Protogen <92652408+StiathirsTheProtogen@users.noreply.github.com>
2022-05-13 11:00:02 -0500

    Add Weapons and Armor pages Also add new MD style syntax Also add links with custom text

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -1,2 +1,4 @@
 document.getElementById("title").innerHTML = document.title.replace(" - Basic RPG Official Wiki","");
+document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\|(.*?)\]\]/g,'<a href="$2">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a href="$1">$1</a>');
+document.body.innerHTML = document.body.innerHTML.replace(/---/g,'<hr>');
