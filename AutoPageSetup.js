f4e71bdabbf4d3436cc11bdb8412959a2c3b3de5
Stiathirs <92652408+Stiathirs@users.noreply.github.com>
2025-06-25 12:34:42 -0500

    Fix.

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -5,10 +5,10 @@ document.getElementById("title").innerHTML = document.title.replace(" - Basic RP
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\|(.*?)\]\]/g,'<a href="$2">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a href="$1">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/---/g,'<hr>');
-document.body.innerHTML = document.body.innerHTML.replace(/^(?:# )(.*)/g,'<h3>$1</h3>');
-document.body.innerHTML = document.body.innerHTML.replace(/^(?:## )(.*)/g,'<h4>$1</h4>');
-document.body.innerHTML = document.body.innerHTML.replace(/^(?:### )(.*)/g,'<h5>$1</h5>');
-document.body.innerHTML = document.body.innerHTML.replace(/^(?:#### )(.*)/g,'<h6>$1</h6>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:# )(.*)/gm,'<h3>$1</h3>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:## )(.*)/gm,'<h4>$1</h4>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:### )(.*)/gm,'<h5>$1</h5>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:#### )(.*)/gm,'<h6>$1</h6>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[(?:b:)(.*)\]/g,'<b>$1</b>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[(?:i:)(.*)\]/g,'<i>$1</i>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[(?:u:)(.*)\]/g,'<u>$1</u>');
