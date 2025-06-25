fb463e61f9faced131e09acb5c72f4114c60349f
Stiathirs <92652408+Stiathirs@users.noreply.github.com>
2025-06-25 12:33:09 -0500

    Changes in formatting in the backend.

--- AutoPageSetup.js
+++ AutoPageSetup.js
@@ -1,7 +1,15 @@
 // Set up page title using element with "title" id
 document.getElementById("title").innerHTML = document.title.replace(" - Basic RPG Official Wiki","");
 
-// Basic MD Rules and using a little MediaWiki syntax
+// Basic MD Rules and a little spicy syntax
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\|(.*?)\]\]/g,'<a href="$2">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\[\[(.*?)\]\]/g,'<a href="$1">$1</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/---/g,'<hr>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:# )(.*)/g,'<h3>$1</h3>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:## )(.*)/g,'<h4>$1</h4>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:### )(.*)/g,'<h5>$1</h5>');
+document.body.innerHTML = document.body.innerHTML.replace(/^(?:#### )(.*)/g,'<h6>$1</h6>');
+document.body.innerHTML = document.body.innerHTML.replace(/\[(?:b:)(.*)\]/g,'<b>$1</b>');
+document.body.innerHTML = document.body.innerHTML.replace(/\[(?:i:)(.*)\]/g,'<i>$1</i>');
+document.body.innerHTML = document.body.innerHTML.replace(/\[(?:u:)(.*)\]/g,'<u>$1</u>');
+document.body.innerHTML = document.body.innerHTML.replace(/\[(?:s:)(.*)\]/g,'<s>$1<s>');
